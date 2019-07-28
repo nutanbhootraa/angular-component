c152c90fb0a1cea333ce26fa67ee39a0869ff0d9
mystery <bhootra1993@gmail.com>
2019-07-28 14:28:57 +0530

    initial commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector : 'app-server',
+  templateUrl : './server.component.html',
+  styles : [`
+  h4 {
+    color: dodgerblue;
+  }
+  `]
+})
+export class ServerComponent {
+
+}
